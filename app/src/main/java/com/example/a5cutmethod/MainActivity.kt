77043cb09c401171cd98eac62ad81e5296ab5755
adam <afhodges@pm.me>
2026-04-07 12:51:43 +0200

    added imperial to metric converter

--- app/src/main/java/com/example/a5cutmethod/MainActivity.kt
+++ app/src/main/java/com/example/a5cutmethod/MainActivity.kt
@@ -18,7 +18,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 
 
@@ -33,6 +35,8 @@ class MainActivity : ComponentActivity() {
             var stripLength by remember { mutableStateOf("") }
             var fenceLength by remember { mutableStateOf("") }
             val result = calculateCut(nearSideWidth, farSideWidth, stripLength, fenceLength)
+            var inches by remember { mutableStateOf("") }
+            inchesToMetric(inches)
 
             Box {
                 Column(
@@ -42,15 +46,18 @@ class MainActivity : ComponentActivity() {
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(
-                        text = "5 cut calculator",
+                        text = "Woodworking helper",
                         modifier = Modifier
                             .padding(bottom = 10.dp),
                         style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
+                        textDecoration = TextDecoration.Underline
                     )
                     Text(
-                        text = "Enter the dimensions in mm",
+                        text = "5 cut calculator",
                         modifier = Modifier
-                        .padding(all = 10.dp)
+                            .padding(bottom = 10.dp),
+                        style = MaterialTheme.typography.titleMedium,
                     )
                     OutlinedTextField(
                         value = nearSideWidth,
@@ -85,12 +92,29 @@ class MainActivity : ComponentActivity() {
                         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
                     )
                     Text(text = result)
+
+                    Text(
+                        text = "Imperial to metric converter",
+                        modifier = Modifier.padding(top = 20.dp),
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                    OutlinedTextField(
+                        value = inches,
+                        onValueChange = { inches = it },
+                        label = { Text("Enter the length in inches:") },
+                        modifier = Modifier
+                            .padding(all = 10.dp),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+                    )
+                    Text(inchesToMetric(inches))
                 }
+
             }
 
         }
     }
 }
+
 
 fun calculateCut(
     nearSideWidth: String,
@@ -113,5 +137,16 @@ fun calculateCut(
         return "the fence should be moved $result mm backwards"
     } else {
         return "No cut needed, the widths are the same"
+    }
+}
+
+fun inchesToMetric(inches: String): String {
+    val inchesNum = inches.toFloatOrNull()
+
+    if (inchesNum == null) {
+        return "Please enter a valid input"
+    } else {
+        val inchToMilResult = "%.1f".format(inchesNum * 25.4)
+        return "$inches inches is $inchToMilResult mm"
     }
 }
